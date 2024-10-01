57f1e5e71c4cb4458d127aeaf060cf12aa97730d
David Chatting <david@davidchatting.com>
2024-10-01 15:54:24 +0100

    basic logic for ws server

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-import fs from 'fs';
 import http from 'node:http'
 import express from 'express'
 import cors from 'cors'
 import process from 'node:process'
 import crypto from 'node:crypto'
-import { WebSocket, WebSocketServer } from 'ws';
+import { WebSocketServer } from 'ws';
 
 const app = express();
 app.use(cors());
@@ -13,40 +12,45 @@ const server = http.createServer(app);
 
 const wss = new WebSocketServer({ server });
 
-const sessions = {
-  'test-session': new Map()
+const sessions = {}
+function addSession(path) {
+  sessions[path] = new Map()
+  return(sessions[path])
 }
 
 wss.on('connection', (ws, req) => {
   console.log(req.url);
 
   try {
-    const match = /session\/(.+)\/?/.exec(req.url)
+    const match = /(.+)\/?/.exec(req.url)
     if (!match) {
       console.error('Invalid socket path', req.url)
       return
     }
 
-    const session = sessions[match[1]]
-    if (!session) {
-      console.error('Invalid session', match[1])
-      return
-    }
+    var session = sessions[match[1]]
+    if (!session) session = addSession(match[1]);
 
     const socketId = crypto.randomUUID()
     console.debug('ws@connection', socketId)
     session.set(socketId, ws)
 
+    console.log('***', session)
+
     ws.on('error', (...args) => console.error('wss@error', ...args));
 
     ws.on('message', (data) => {
       try {
-        console.debug('wss@message: %s', data);
-        const { type, ...payload } = JSON.parse(data)
-
-        for (const [id, socket] of session.entries()) send(socket, id, {type,...payload})
-
-      } catch (error) {
+        const json = JSON.parse(data)
+        
+        for (const [id, socket] of session.entries()) {
+          if(id !== json['sender']) {
+            console.log("*", id, json)
+            send(socket, id, json)
+          }
+        }
+      }
+      catch (error) {
         console.error('wss@message - invalid payload', error)
       }
     });
